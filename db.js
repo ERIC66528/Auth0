d89436f9f1dcb6f59ef91ba2435df3a53beb7215
Eric <mateieric66@gmail.com>
2025-09-22 13:48:11 +0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,19 @@
+import { app } from "./index.html";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+
+const db = getFirestore(app);
+
+export async function saveUserData(user) {
+  try {
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name: user.displayName,
+      email: user.email,
+      photoURL: user.photoURL,
+      createdAt: new Date().toISOString()
+    });
+    console.log("User saved to Firestore");
+  } catch (e) {
+    console.error("Error saving user:", e);
+  }
+}
