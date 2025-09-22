ae4470a80e28adae17e402c2275e7a15841904df
Eric <mateieric66@gmail.com>
2025-09-22 13:47:38 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,22 @@
+import { app } from "./index.html";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { saveUserData } from "./db.js";
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+document.getElementById("login").addEventListener("click", () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+
+      // Save user to Firestore
+      saveUserData(user);
+
+      // Redirect after login
+      window.location.href = "https://eric66528.github.io/E-commerceweb/";
+    })
+    .catch((error) => {
+      console.error("Login failed:", error.message);
+    });
+});
